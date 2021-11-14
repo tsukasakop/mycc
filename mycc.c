36c5b05c005064f0aa9eb864c51dd1a2049a54d3
tkskp_sub <tkskp_sub@outlook.jp>
2021-11-14 21:01:09 +0900

    エラー文を修正

--- mycc.c
+++ mycc.c
@@ -25,6 +25,23 @@ struct Token {
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
 // エラーを報告するための関数
 // printfと同じ引数を取る
 void error(char *fmt, ...) {
@@ -48,7 +65,8 @@ bool consume(char op) {
 // それ以外の場合にはエラーを報告する。
 void expect(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
-    error("'%c'ではありません", op);
+    // error("'%c'ではありません", op);
+    error_at(token->str, "'%c'ではありません", op);
   token = token->next;
 }
 
@@ -56,7 +74,8 @@ void expect(char op) {
 // それ以外の場合にはエラーを報告する。
 int expect_number() {
   if (token->kind != TK_NUM)
-    error("数ではありません");
+    // error("数ではありません");
+    error_at(token->str, "数ではありません");
   int val = token->val;
   token = token->next;
   return val;
@@ -99,7 +118,8 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    error("トークナイズできません");
+    // error("トークナイズできません");
+    error_at(p, "トークナイズできません");
   }
 
   new_token(TK_EOF, cur, p);
@@ -109,8 +129,11 @@ Token *tokenize(char *p) {
 int main(int argc, char **argv) {
   if (argc != 2) {
     error("引数の個数が正しくありません");
+    // error(token->str, "引数の個数が正しくありません");
     return 1;
   }
+
+  user_input = argv[1];
 
   // トークナイズする
   token = tokenize(argv[1]);
